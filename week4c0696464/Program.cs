6b40864569bef5777bb9fdddf3947942e599c5eb
visualstudio <visualstudio@230-PC37>
2019-01-31 14:10:05 -0500

    week4

--- week4c0696464/Program.cs
+++ week4c0696464/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace week4c0696464
 {
-    class Program
+    class program
     {
-        static void Main(string[] args)
+        public program()
         {
         }
     }
+
+    class Village
+{
+    public static int numberOfVillages = 0;
+    public Village nextVillage;
+    public Village previousVillage;
+    public string VillageNmae;
+    public bool isAstrildeHere = false;
+        private bool isAstrildHere;
+
+        public Village()
+    {
+        Village.numberOfVillages++;
+    }
+
+        class Countryside
+        {
+            public Village Maple;
+            public Village Toronto;
+            public Village Ajax;
+
+            
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.VillageName = "Maple";
+                Maple.previousVillage = null;
+                Maple.nextVillage = Toronto;
+
+                Toronto = new Village();
+                Toronto.VillageName = "Toronto";
+                Toronto.previousVillage = Maple;
+                Toronto.nextVillage = Ajax;
+
+
+                Ajax = new Village();
+                Ajax.VillageName = "Ajax";
+                Ajax.previousVillage = Toronto;
+                Ajax.nextVillage = null;
+                Ajax.isAstrildHere = true;
+
+
+            }
+
+            public void
+        }
+
 }
+       
+ }
+ 
